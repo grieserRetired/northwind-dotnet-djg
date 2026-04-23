754713d10c9f191be7628596ca67220bd6b87c2d
grieserRetired <grieserRetired@outlook.com>
2026-04-23 10:43:18 -0700

    Read API URL from environment variable for Railway deployment

--- NorthwindDotNet.Web/Program.cs
+++ NorthwindDotNet.Web/Program.cs
@@ -1,29 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddRazorPages();
 
+var apiBaseUrl = Environment.GetEnvironmentVariable("ApiBaseUrl") ?? "http://localhost:5132";
 builder.Services.AddHttpClient("NorthwindApi", client =>
 {
-    client.BaseAddress = new Uri("http://localhost:5132");
+    client.BaseAddress = new Uri(apiBaseUrl);
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
